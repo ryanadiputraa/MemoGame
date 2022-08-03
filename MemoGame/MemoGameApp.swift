91917e402045796ddcfff4fbd7749452b0b249bd
Ryan Adi Putra <ryannadiputraa@gmail.com>
2022-08-03 21:05:16 +0800

    Initial Commit

---
+++ MemoGame/MemoGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoGameApp.swift
+//  MemoGame
+//
+//  Created by Ryan Adi Putra on 03/08/22.
+//
+
+import SwiftUI
+
+@main
+struct MemoGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
