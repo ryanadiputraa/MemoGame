32512ea3258dcdaadabfe9f5bbf1eb02b8f3019a
Ryan Adi Putra <ryannadiputraa@gmail.com>
2022-08-16 21:19:58 +0800

    Deck transition animation

--- MemoGame/EmojiMemoGameView.swift
+++ MemoGame/EmojiMemoGameView.swift
@@ -9,10 +9,38 @@ import SwiftUI
 
 struct EmojiMemoGameView: View {
     @ObservedObject var game: EmojiMemoGame
+    @Namespace private var dealingNamespace
+    @State private var dealt = Set<UUID>()
+    
+    private struct CardConstants {
+        static let color = Color.red
+        static let aspectRatio: CGFloat = 2/3
+        static let dealDuration: Double = 0.5
+        static let totalDuration: Double = 2
+        static let undealtHeight: CGFloat = 90
+        static let undealtWidth = undealtHeight * aspectRatio
+    }
+    
+    private func deal(_ card: EmojiMemoGame.Card) {
+        dealt.insert(card.id)
+    }
+    
+    private func isUndealt(_ card: EmojiMemoGame.Card) -> Bool {
+        !dealt.contains(card.id)
+    }
+    
+    private func dealAnimation(for card: EmojiMemoGame.Card) -> Animation {
+        var delay = 0.0
+        if let index = game.cards.firstIndex(where: { $0.id == card.id }) {
+            delay = Double(index) * (CardConstants.totalDuration / Double(game.cards.count))
+        }
+        return Animation.easeInOut(duration: CardConstants.dealDuration).delay(delay)
+    }
     
     var body: some View {
         VStack {
             gameBody
+            deckBody
             shuffleButton
         }
         .padding()
@@ -20,11 +48,13 @@ struct EmojiMemoGameView: View {
     
     var gameBody: some View {
         AspectVGrid(items: game.cards, aspectRatio: 2/3) { card in
-            if card.isMatched && !card.isFaceUp {
+            if isUndealt(card) || card.isMatched && !card.isFaceUp {
                 Color.clear
             } else {
                 CardView(card: card)
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
                     .padding(4)
+                    .transition(.asymmetric(insertion: .identity, removal: .scale))
                     .onTapGesture {
                         withAnimation {
                             game.choose(card)
@@ -32,7 +62,26 @@ struct EmojiMemoGameView: View {
                     }
             }
         }
-        .foregroundColor(.red)
+        .foregroundColor(CardConstants.color)
+    }
+    
+    var deckBody: some View {
+        ZStack {
+            ForEach(game.cards.filter(isUndealt)) { card in
+                CardView(card: card)
+                    .matchedGeometryEffect(id: card.id, in: dealingNamespace)
+                    .transition(.asymmetric(insertion: .opacity, removal: .identity))
+            }
+        }
+        .onTapGesture {
+            for card in game.cards {
+                withAnimation(dealAnimation(for: card)) {
+                    deal(card)
+                }
+            }
+        }
+        .frame(width: CardConstants.undealtWidth, height: CardConstants.undealtHeight)
+        .foregroundColor(CardConstants.color)
     }
     
     var shuffleButton: some View {
